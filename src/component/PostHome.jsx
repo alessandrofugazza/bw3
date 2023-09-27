1e704ac7088f916c931957503ca26d60bb8fc01f
Flavio Neri <flavio.neri00@gmail.com>
2023-09-27 16:38:46 +0200

    Update PostHome.jsx

--- src/component/PostHome.jsx
+++ src/component/PostHome.jsx
@@ -10,6 +10,7 @@ const PostHome = () => {
           <img
             src="https://images.unsplash.com/photo-1695642579133-d41f36bcca61?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"
             alt="img-profile"
+            className="rounded-circle"
           />
         </Col>
         <Col xs={11}>
