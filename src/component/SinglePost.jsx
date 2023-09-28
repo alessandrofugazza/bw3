6877ba6c89cf39c8a3d213cfb6175071dddd246b
Flavio Neri <flavio.neri00@gmail.com>
2023-09-28 11:04:01 +0200

    Link for profiles

--- src/component/SinglePost.jsx
+++ src/component/SinglePost.jsx
@@ -2,10 +2,13 @@ import { Card } from "react-bootstrap";
 import { BiPencil } from "react-icons/bi";
 import ModalPutDelPost from "./ModalPutDelPost";
 import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const SinglePost = ({ post }) => {
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
+
   return (
     <>
       <Card className="my-2">
@@ -13,16 +16,22 @@ const SinglePost = ({ post }) => {
           className="d-flex
         "
         >
-          <img
-            src={post.user.image}
-            alt="img-profile"
-            style={{ width: "50px", height: "50px", borderRadius: "50%", margin: "10px" }}
-          />
+          <Link to={`/${post.user._id}`}>
+            <img
+              src={post.user.image}
+              alt="img-profile"
+              style={{ width: "50px", height: "50px", borderRadius: "50%", margin: "10px" }}
+            />
+          </Link>
           <Card.Body>
             <div className="d-flex justify-content-between align-items-center">
               {" "}
-              <Card.Title>{post.username}</Card.Title>{" "}
-              <BiPencil onClick={() => handleShow()} className="fs-3 text-secondary" />
+              <Link to={`/${post.user._id}`} className="nav-link">
+                <Card.Title>{post.username}</Card.Title>{" "}
+              </Link>
+              {post.user._id === "651139bb3752a80014568765" && (
+                <BiPencil onClick={() => handleShow()} className="fs-3 text-secondary" />
+              )}
             </div>
             <Card.Subtitle className="mb-2 text-muted">{post.user.title}</Card.Subtitle>
             <Card.Text>{post.text}</Card.Text>
