36b8e97a4e8e078ef401f6b31a378dd20228c544
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-09-29 16:04:13 +0200

    ...

--- src/component/ModalPost.jsx
+++ src/component/ModalPost.jsx
@@ -7,13 +7,15 @@ const ModalPost = ({ show, setShow }) => {
   const handleClose = () => setShow(false);
   const [textPost, setTextPost] = useState("");
   const dispatch = useDispatch();
+  const [newPost, setNewPost] = useState("");
 
   const changePost = (valore) => {
     setTextPost(valore);
   };
-
-  const fetchPostArticle = async () => {
-    const resp = await fetch("https://striveschool-api.herokuapp.com/api/posts/", {
+  const handleSubmit = async (e) => {
+    const formData = new FormData();
+    formData.append("post", e.target[1].files[0]);
+    const resp1 = await fetch("https://striveschool-api.herokuapp.com/api/posts/", {
       method: "POST",
       headers: {
         Authorization:
@@ -22,8 +24,20 @@ const ModalPost = ({ show, setShow }) => {
       },
       body: JSON.stringify({ text: textPost }),
     });
-    handleClose();
+    if (resp1.ok) {
+      const data = await resp1.json();
+
+      const resp2 = await fetch("https://striveschool-api.herokuapp.com/api/posts/" + data._id, {
+        method: "POST",
+        body: formData,
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTExMzliYjM3NTJhODAwMTQ1Njg3NjUiLCJpYXQiOjE2OTU2Mjc3MDcsImV4cCI6MTY5NjgzNzMwN30.4BcdJm9NGzCRCfUXd__fN8D0mZG4DURnYc4zl0Oh6Uk",
+        },
+      });
+    }
     dispatch(fetchAllPost());
+    handleClose();
   };
 
   return (
@@ -31,23 +45,35 @@ const ModalPost = ({ show, setShow }) => {
       <Modal.Header closeButton>
         <Modal.Title></Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <FloatingLabel controlId="floatingTextarea2" label="Di cosa vorresti parlare?">
-          <Form.Control
-            onChange={(e) => {
-              changePost(e.target.value);
-            }}
-            as="textarea"
-            placeholder="Di cosa vorresti parlare?"
-            style={{ height: "100px" }}
-          />
-        </FloatingLabel>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="outline-primary" onClick={() => fetchPostArticle()}>
-          Posta Attività
-        </Button>
-      </Modal.Footer>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          handleSubmit(e);
+        }}
+      >
+        <Modal.Body>
+          <FloatingLabel controlId="floatingTextarea2" label="Di cosa vorresti parlare?">
+            <Form.Control
+              onChange={(e) => {
+                changePost(e.target.value);
+              }}
+              as="textarea"
+              placeholder="Di cosa vorresti parlare?"
+              style={{ height: "100px" }}
+            />
+          </FloatingLabel>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>Inserisci un'immagine per il post</Form.Label>
+            <Form.Control type="file" />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button type="submit" variant="outline-primary">
+            Posta Attività
+          </Button>
+        </Modal.Footer>
+      </Form>
     </Modal>
   );
 };
